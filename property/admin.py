3f9e3ffce1d66cd82001545cb87694c85529b0fd
Andrei K <1____1@bk.ru>
2019-12-16 16:55:41 +0300

    link flats with owners, delete unecessary fields

--- property/admin.py
+++ property/admin.py
@@ -4,13 +4,13 @@ from .models import Complaint, Flat, Owner
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ("address", "town", "owner")
+    search_fields = ("address", "town")
     readonly_fields = ("created_at",)
     list_display = ("address", "price", "new_building",
                     "construction_year", "town")
     list_editable = ("new_building",)
     list_filter = ("new_building", "rooms_number", "has_balcony", "active")
-    raw_id_fields = ("liked_by", )
+    raw_id_fields = ("liked_by", "owner")
 
 
 class ComplaintAdmin(admin.ModelAdmin):
--- property/models.py
+++ property/models.py
@@ -5,10 +5,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner_depricated = models.CharField("ФИО владельца", max_length=200)
-    owners_phonenumber = models.CharField("Номер владельца", max_length=20)
+    owner = models.ManyToManyField("Owner", verbose_name='Владелец')
     new_building = models.NullBooleanField("Новое здание")
-    owners_phone_pure = PhoneNumberField('Нормализованный номер', blank=True, null=True)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
     
     description = models.TextField("Текст объявления", blank=True)
